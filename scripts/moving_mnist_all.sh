ae100213956959efdad65df6a7cc7d0a8da664ab
Quan Nguyen <qmnguyen@iu.edu>
2023-05-11 15:56:33 -0400

    Fix wrong file name

--- scripts/moving_mnist_all.sh
+++ scripts/moving_mnist_all.sh
@@ -10,9 +10,9 @@ module load deeplearning
 # Run moving_mnist_predrnn .py script as .ipynb notebook
 # and store the result notebook.
 conda activate predrnn
-cat examples/moving_mnist_predrnn_all.py \
+cat examples/moving_mnist_all.py \
     | jupytext --to ipynb \
-    | papermill - "examples/moving_mnist_predrnn_all_out_$model_"$use_ss"_$SLURM_JOBID.ipynb" \
-    -p model_save_path "./saved_models/moving_mnist_predrnn_all_$model_"$use_ss"_$SLURM_JOBID" \
+    | papermill - "examples/moving_mnist_all_out_$model_"$use_ss"_$SLURM_JOBID.ipynb" \
+    -p model_save_path "./saved_models/moving_mnist_all_$model_"$use_ss"_$SLURM_JOBID" \
     -p model_type $model \
     -p use_reversed_sampling $use_ss
